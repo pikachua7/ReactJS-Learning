f2e50236eb5859364a2eed67963abbcd15d7387c
pikachua7 <atharvapaliwal7@gmail.com>
2023-11-03 08:23:08 +0530

    setup 03 from 02

---
+++ 03/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/*
+
+Creating a structure like - 
+
+<div id="parent">
+  <div id="children">
+      <h1>I'm h1 tag</h1>
+      <h2>I'm h2 tag</h2>
+  </div>
+</div>
+
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    {
+      id: "child",
+    },
+    [
+      React.createElement("h1", {}, "I'm h1 tag"),
+      React.createElement("h2", {}, "I'm h2 tag"),
+    ]
+  )
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
