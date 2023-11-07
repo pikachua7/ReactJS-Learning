f29f654fb6bfd5ee9b0ae9123ffd468a20539c6f
pikachua7 <atharvapaliwal7@gmail.com>
2023-11-07 09:41:21 +0530

    adding a header component

--- 03/App.js
+++ 03/App.js
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom/client";
 
 const jsxHeading = <h1 id="heading">Hello From JSX!</h1>;
 
+const HeaderComponent = () => {
+  return (
+    <div>
+      <h1 id="heading">Hello from React Functional Component!</h1>
+    </div>
+  );
+};
+
+console.log(HeaderComponent()); // Gives the object
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+// root.render(HeaderComponent()); This also works
+
+root.render(<HeaderComponent />);
