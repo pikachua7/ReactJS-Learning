6a4ac332d4b7ed14e23f4ee1908f57c6c931d9d6
pikachua7 <atharvapaliwal7@gmail.com>
2023-11-07 09:49:39 +0530

    nested components : component composition

--- 03/App.js
+++ 03/App.js
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom/client";
 
 const jsxHeading = <h1 id="heading">Hello From JSX!</h1>;
 
+const TitleComponent = () => {
+  return <h1>Hello From Title!</h1>;
+};
+
+// Calling one component inside another component : Component Composition
+
 const HeaderComponent = () => {
   return (
     <div>
+      <TitleComponent />
       <h1 id="heading">Hello from React Functional Component!</h1>
     </div>
   );
