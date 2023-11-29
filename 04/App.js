7a08f0e7e27fd381771b756f5fc2c4d18e11886b
pikachua7 <atharvapaliwal7@gmail.com>
2023-11-29 07:59:25 +0530

    add header component

--- 04/App.js
+++ 04/App.js
@@ -1,5 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// Header Component
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src="https://images-platform.99static.com/CDG3tPS1HqvP7u8kJysYXIf-os4=/500x500/top/smart/99designs-contests-attachments/58/58659/attachment_58659944"
+          alt=""
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+// Top Level Component
+const AppLayout = () => {
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+      {/* Body */}
+      {/* Footer */}
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<AppLayout />);
