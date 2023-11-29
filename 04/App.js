39c649c812b55cfbb038df70fedaed77b273fc76
pikachua7 <atharvapaliwal7@gmail.com>
2023-11-29 08:03:55 +0530

    add css and logo class

--- 04/App.js
+++ 04/App.js
@@ -8,6 +8,7 @@ const Header = () => {
     <div className="header">
       <div className="logo-container">
         <img
+          className="logo"
           src="https://images-platform.99static.com/CDG3tPS1HqvP7u8kJysYXIf-os4=/500x500/top/smart/99designs-contests-attachments/58/58659/attachment_58659944"
           alt=""
         />
