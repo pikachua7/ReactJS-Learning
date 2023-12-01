d9ab16ed9faa00cd7424abdcddf03358a090e019
pikachua7 <atharvapaliwal7@gmail.com>
2023-12-01 23:41:05 +0530

    setup project for ep 6

---
+++ 06/src/components/Body.js
@@ -0,0 +1,29 @@
+// Restaurant Card
+import { RestaurantCard } from "./RestaurantCard";
+import { resList } from "../utils/mockData";
+import { useState } from "react";
+
+export const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            setListOfRestaurants(
+              listOfRestaurants.filter((res) => res.info.avgRating > 4)
+            );
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
