fe568424eb40234324e9d6b2fed899e9c16cd3b8
pikachua7 <atharvapaliwal7@gmail.com>
2023-12-03 17:23:14 +0530

    add login logout for understanding useState

--- 06/src/components/Header.js
+++ 06/src/components/Header.js
@@ -1,6 +1,15 @@
 import { IMG_URL } from "../utils/constant";
+import { useState } from "react";
 
 export const Header = () => {
+  const [authTextBtn, setAuthTextBtn] = useState("Login");
+
+  const handleAuthTextBtn = () => {
+    authTextBtn === "Login"
+      ? setAuthTextBtn("Logout")
+      : setAuthTextBtn("Login");
+  };
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +21,9 @@ export const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button className="auth-btn" onClick={handleAuthTextBtn}>
+            {authTextBtn}
+          </button>
         </ul>
       </div>
     </div>
