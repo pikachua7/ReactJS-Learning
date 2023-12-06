58e81ec3dc9d5e6ccff09bd98a5b2b0cf169e83c
pikachua7 <atharvapaliwal7@gmail.com>
2023-12-07 00:19:31 +0530

    add url as constant

--- 07/src/utils/constant.js
+++ 07/src/utils/constant.js
@@ -3,3 +3,6 @@ export const CDN_URL =
 
 export const IMG_URL =
   "https://images-platform.99static.com/CDG3tPS1HqvP7u8kJysYXIf-os4=/500x500/top/smart/99designs-contests-attachments/58/58659/attachment_58659944";
+
+export const MENU_API_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.3164945&lng=78.03219179999999&restaurantId=";
