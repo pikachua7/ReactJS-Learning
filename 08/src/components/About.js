b57d357307a41b980cb8762dc367cbf0ab726a1c
pikachua7 <atharvapaliwal7@gmail.com>
2023-12-11 21:01:03 +0530

    add user and user class component in about

--- 08/src/components/About.js
+++ 08/src/components/About.js
@@ -1,3 +1,11 @@
+import { User } from "./User";
+import { UserClassComponent } from "./UserClassComponent";
+
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <User />
+      <UserClassComponent />
+    </div>
+  );
 };
