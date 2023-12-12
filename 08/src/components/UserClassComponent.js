7c486616de30fab536673372e8c7b319c5be65dc
pikachua7 <atharvapaliwal7@gmail.com>
2023-12-12 07:27:19 +0530

    destructure class component props

--- 08/src/components/UserClassComponent.js
+++ 08/src/components/UserClassComponent.js
@@ -5,9 +5,10 @@ export class UserClassComponent extends React.Component {
     super(props);
   }
   render() {
+    const { name } = this.props;
     return (
       <div className="user-card">
-        <h2>Name : {this.props.name}</h2>
+        <h2>Name : {name}</h2>
         <h3>Location</h3>
         <h4>Contact</h4>
       </div>
