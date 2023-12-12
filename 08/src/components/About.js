86f8c6a9bc2ac2672c782ef822c47913c31039c3
pikachua7 <atharvapaliwal7@gmail.com>
2023-12-12 07:53:37 +0530

    update state variable in class component

--- 08/src/components/About.js
+++ 08/src/components/About.js
@@ -4,7 +4,7 @@ import { UserClassComponent } from "./UserClassComponent";
 export const About = () => {
   return (
     <div>
-      <User name={"Atharva from Functional component"} />
+      {/* <User name={"Atharva from Functional component"} /> */}
       <UserClassComponent name={"Atharva from Class component"} />
     </div>
   );
--- 08/src/components/UserClassComponent.js
+++ 08/src/components/UserClassComponent.js
@@ -8,13 +8,19 @@ export class UserClassComponent extends React.Component {
       count: 0,
     };
   }
+
   render() {
     const { name } = this.props;
     const { count } = this.state;
 
+    const handleClick = () => {
+      this.setState({ count: count + 1 });
+    };
+
     return (
       <div className="user-card">
         <h4>Count: {count}</h4>
+        <button onClick={handleClick}>Click Me!</button>
         <h2>Name: {name}</h2>
         <h3>Location</h3>
         <h4>Contact</h4>
