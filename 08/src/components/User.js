302c3f5036178fb934914d95a24330f693067dca
pikachua7 <atharvapaliwal7@gmail.com>
2023-12-12 07:39:04 +0530

    add state variables in class and functional components

--- 08/src/components/User.js
+++ 08/src/components/User.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 export const User = ({ name }) => {
+  const [count, setCount] = useState(0);
   return (
     <div className="user-card">
+      <h4>Count: {count}</h4>
       <h2>Name: {name}</h2>
       <h3>Location</h3>
       <h4>Contact</h4>
--- 08/src/components/UserClassComponent.js
+++ 08/src/components/UserClassComponent.js
@@ -3,12 +3,19 @@ import React from "react";
 export class UserClassComponent extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+    };
   }
   render() {
     const { name } = this.props;
+    const { count } = this.state;
+
     return (
       <div className="user-card">
-        <h2>Name : {name}</h2>
+        <h4>Count: {count}</h4>
+        <h2>Name: {name}</h2>
         <h3>Location</h3>
         <h4>Contact</h4>
       </div>
