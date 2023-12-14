f7299e59d3435ae2c842b899fb53c11b1491adb7
pikachua7 <atharvapaliwal7@gmail.com>
2023-12-14 07:47:33 +0530

    make use of github api

--- 08/src/components/UserClassComponent.js
+++ 08/src/components/UserClassComponent.js
@@ -6,18 +6,28 @@ export class UserClassComponent extends React.Component {
 
     this.state = {
       count: 0,
+      userInfo: {
+        name: "",
+        avatar_url: "",
+      },
     };
 
     // console.log(this.props.name + " " + "Child Constructor");
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     // console.log(this.props.name + " " + "Inside Child Component Did Mount");
+
+    const data = await fetch("https://api.github.com/users/pikachua7");
+    const json = await data.json();
+    this.setState({
+      userInfo: json,
+    });
   }
 
   render() {
     // console.log(this.props.name + " " + "Child Render");
-    const { name } = this.props;
+    const { name, avatar_url } = this.state.userInfo;
     const { count } = this.state;
 
     const handleClick = () => {
@@ -28,6 +38,7 @@ export class UserClassComponent extends React.Component {
       <div className="user-card">
         <h4>Count: {count}</h4>
         <button onClick={handleClick}>Click Me!</button>
+        <img src={avatar_url} />
         <h2>Name: {name}</h2>
         <h3>Location</h3>
         <h4>Contact</h4>
