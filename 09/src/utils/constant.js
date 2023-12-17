c19f798776f509f6d7aa6d096cb357e22a89493a
pikachua7 <atharvapaliwal7@gmail.com>
2023-12-17 17:22:21 +0530

    add restaurant list url to constant

--- 09/src/utils/constant.js
+++ 09/src/utils/constant.js
@@ -6,3 +6,6 @@ export const IMG_URL =
 
 export const MENU_API_URL =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.3164945&lng=78.03219179999999&restaurantId=";
+
+export const RESTAURANT_LIST_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.3164945&lng=78.03219179999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
