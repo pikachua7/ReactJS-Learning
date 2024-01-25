aa24531bb3cc375f44a53a83da3dd627cb36067b
pikachua7 <atharvapaliwal7@gmail.com>
2024-01-26 00:54:46 +0530

    toggle feature for accordian

--- 11/src/components/RestaurantCategory.js
+++ 11/src/components/RestaurantCategory.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import { ItemList } from "./ItemList";
 
 export const RestaurantCategory = ({ category }) => {
+  const [showItem, setShowItem] = useState(false);
+  const handleClick = () => {
+    setShowItem(!showItem);
+  };
   return (
     <div>
       {/* Accordian Header */}
       <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
-        <div className="flex justify-between">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
           <span className="font-bold text-lg">
             {category.title} ({category.itemCards.length})
           </span>
@@ -13,7 +21,7 @@ export const RestaurantCategory = ({ category }) => {
         </div>
 
         {/* Accordian Body */}
-        <ItemList items={category.itemCards} />
+        {showItem && <ItemList items={category.itemCards} />}
       </div>
     </div>
   );
