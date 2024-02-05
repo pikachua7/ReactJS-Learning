e67b19338c3cd777af1dcd809ded484efe82d3c0
pikachua7 <atharvapaliwal7@gmail.com>
2024-02-05 09:54:52 +0530

    add reducer to the store parent reducer

--- 12/src/utils/appStore.js
+++ 12/src/utils/appStore.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./cartSlice";
 
 export const appStore = configureStore({
   // Add Slices inside here
+  // This reducer contains the reducers of each slice in the store we need
+  reducer: {
+    cart: cartSlice,
+  },
 });
